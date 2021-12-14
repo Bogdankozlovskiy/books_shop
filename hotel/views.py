38f0b68503889b34f1281bff4f32d0e9e8077f02
bogdankozlovsky <bogdan.k@datarails.com>
2021-12-14 18:47:48 +0100

    finished implementation of filter room

--- hotel/views.py
+++ hotel/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from hotel.models import Room, OrderRoom
 from django.utils import timezone
 from datetime import datetime
+from django.db.models import Q, F
 
 
 def search_room(request):
     if not request.GET:
         return render(request, "search_room.html")
-    datetime(year=2021, month=12, day=9, hour=23, tzinfo=timezone.get_default_timezone())
     start_date = request.GET.get("start_date")
     start_date = datetime.strptime(start_date, "%Y-%m-%d")
     start_date = start_date.astimezone(timezone.get_current_timezone())
@@ -15,7 +15,10 @@ def search_room(request):
     end_date = datetime.strptime(end_date, "%Y-%m-%d")
     end_date = end_date.astimezone(timezone.get_current_timezone())
 
-    ordered_case_1 = OrderRoom.objects.filter(start_date__gt=start_date, end_date__lt=end_date)
-    ordered_case_2 = OrderRoom.objects.filter(start_date__lt=start_date, end_date__lt=end_date)
-    free_rooms = Room.objects.exclude(ordered__in=ordered_case_1).exclude(ordered__in=ordered_case_2)
+    free_rooms = Room.objects.filter(
+        ~Q(ordered__in=OrderRoom.objects.filter(start_date__gt=start_date, end_date__lt=end_date))&
+        ~Q(ordered__in=OrderRoom.objects.filter(start_date__gt=start_date, end_date__gt=end_date, start_date__lt=end_date))&
+        ~Q(ordered__in=OrderRoom.objects.filter(start_date__lt=start_date, end_date__lt=end_date, end_date__gt=start_date))&
+        ~Q(ordered__in=OrderRoom.objects.filter(start_date__lt=start_date, end_date__gt=end_date))
+    ).annotate(price=F("price_for_day") * (end_date - start_date).days)
     return render(request, "show_rooms.html", {"free_rooms": free_rooms})
