7ec4bddc3bb1fabcd90bcd2c21aaf8a7fbd9f055
bogdankozlovsky <bogdan.k@datarails.com>
2021-12-26 00:10:01 +0100

    add version for django-debug-toolbar

---
+++ main.py
@@ -0,0 +1,37 @@
+import websockets
+import asyncio
+from websockets.exceptions import ConnectionClosedOK
+
+
+all_client = []
+
+
+async def send_message(message):
+    for client in all_client:
+        try:
+            await client.send(message)
+        except ConnectionClosedOK:
+            all_client.remove(client)
+
+
+async def accept_new_client(client_socket, path):
+    print("new client connected")
+    all_client.append(client_socket)
+    while True:
+        try:
+            new_message = await client_socket.recv()
+        except ConnectionClosedOK:
+            await send_message("one client live us")
+            break
+        print("client send:", new_message)
+        await send_message(new_message)
+
+
+async def start_server():
+    print("starting server")
+    await websockets.serve(accept_new_client, "localhost", 5555)
+
+
+event_loop = asyncio.get_event_loop()
+event_loop.run_until_complete(start_server())
+event_loop.run_forever()
