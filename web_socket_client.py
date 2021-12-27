a2cb3a379c2bb61203d552ee7447755883002d0a
bogdankozlovsky <bogdan.k@datarails.com>
2021-12-27 16:30:10 +0100

    change websocket client

--- web_socket_client.py
+++ web_socket_client.py
@@ -3,8 +3,8 @@ from websockets import connect
 
 
 async def hello(uri):
-    async with connect(uri) as websocket:
-        await websocket.send("Hello world!")
-        return await websocket.recv()
+    websocket = await connect(uri)
+    await websocket.send("Hello world!")
+    return await websocket.recv()
 
 asyncio.run(hello("ws://localhost:5555"))
