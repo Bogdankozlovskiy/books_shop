2fe10f8b08e61e304a4f9d854578f508f437bf04
bogdankozlovsky <bogdan.k@datarails.com>
2022-01-11 21:24:22 +0100

    fix asgi file

--- bookshop/asgi.py
+++ bookshop/asgi.py
@@ -2,12 +2,14 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from web_chat.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookshop.settings')
+django_asgi_app = get_asgi_application()
+
+from web_chat.routing import websocket_urlpatterns  # we can locad models and other thinks only after django.setup()
 
 application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
+  "http": django_asgi_app,
   "websocket": AuthMiddlewareStack(
         URLRouter(
             websocket_urlpatterns
