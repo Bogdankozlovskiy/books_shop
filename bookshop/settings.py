8bbade7d484206d325ce79cf068bfe311b6ba564
bogdankozlovsky <bogdan.k@datarails.com>
2022-01-21 13:18:11 +0100

    add celery

--- bookshop/settings.py
+++ bookshop/settings.py
@@ -94,8 +94,12 @@ ASGI_APPLICATION = 'bookshop.asgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_celery',
+        "USER": "django_user",
+        "PASSWORD": "django_pwd",
+        "HOST": "localhost",
+        "PORT": 5432
     }
 }
 
